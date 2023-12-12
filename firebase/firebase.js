6e995778a9f4e20f53d82c0528404c58702382fa
Mandella90 <natestrehlow@yahoo.com>
2023-12-12 15:55:14 -0600

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBS7MOHsT2kyr_l3NK9v3fuVN5_ZQseKrY",
-  authDomain: "whereimatchat.firebaseapp.com",
-  projectId: "whereimatchat",
-  storageBucket: "whereimatchat.appspot.com",
-  messagingSenderId: "873678375914",
-  appId: "1:873678375914:web:6469493e55059539504093",
-  measurementId: "G-YCKV0BWYRR"
+  apiKey: "",
+  authDomain: ".com",
+  projectId: "",
+  storageBucket: ".appspot.com",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 
 if (!firebase.apps.length) {
@@ -19,26 +19,4 @@ export { firebase }
 
 
 
-
-
-
-/*import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
-import 'firebase/compat/firestore'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBS7MOHsT2kyr_l3NK9v3fuVN5_ZQseKrY",
-  authDomain: "whereimatchat.firebaseapp.com",
-  projectId: "whereimatchat",
-  storageBucket: "whereimatchat.appspot.com",
-  messagingSenderId: "873678375914",
-  appId: "1:873678375914:web:6469493e55059539504093",
-  measurementId: "G-YCKV0BWYRR"
-}
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-}
-export { firebase }
-*/
 
